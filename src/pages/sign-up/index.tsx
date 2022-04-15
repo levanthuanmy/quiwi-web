9a8c3a4060424b62ae97da26549a507a19c91364
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2022-04-16 00:34:52 +0700

    get sign up form

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,3 +1,4 @@
+import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
@@ -5,32 +6,89 @@ import MyButton from '../../components/MyButton/MyButton'
 import MyInput from '../../components/MyInput/MyInput'
 import SingleFloatingCardLayout from '../../components/SingleFloatingCardLayout/SingleFloatingCardLayout'
 
+type SignUpForm = {
+  username: string
+  password: string
+}
+
 const SignUpPage: NextPage = () => {
   const router = useRouter()
+  const initialValues: SignUpForm = { username: '', password: '' }
+
+  const onSignUp = async (
+    body: SignUpForm,
+    actions: FormikHelpers<SignUpForm>
+  ) => {
+    try {
+      const res = await fetch(`http://quiwi.games:3000/api/auth/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      console.log('res', await res.json())
+    } catch (error) {
+      console.log('onSignUp - error', error)
+    } finally {
+      actions.setSubmitting(false)
+    }
+  }
+
+  const handleSignUpFormSubmit = (
+    values: SignUpForm,
+    actions: FormikHelpers<SignUpForm>
+  ) => {
+    console.log('values', JSON.stringify(values))
+    onSignUp(values, actions)
+  }
+
   return (
     <SingleFloatingCardLayout>
       <div className="py-32px fs-32px fw-medium">Đăng ký</div>
+      <Formik initialValues={initialValues} onSubmit={handleSignUpFormSubmit}>
+        {({
+          // values,
+          // errors,
+          // touched,
+          // handleChange,
+          // handleBlur,
+          // handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form>
+            <Field
+              type="text"
+              name="username"
+              placeholder="Tên tài khoản"
+              as={MyInput}
+              iconClassName="bi bi-person"
+              className="mb-3"
+            />
+            <Field
+              type="password"
+              name="password"
+              placeholder="Mật khẩu"
+              as={MyInput}
+              iconClassName="bi bi-unlock"
+              className="mb-3"
+            />
+            {/* <Field
+              type="text"
+              name="password"
+              placeholder="Xác nhận mật khẩu"
+              as={MyInput}
+              iconClassName="bi bi-unlock"
+            /> */}
 
-      <MyInput
-        className="mb-3"
-        type="text"
-        placeholder="Email"
-        iconClassName="bi bi-at"
-      />
-      <MyInput
-        className="mb-3"
-        type="password"
-        placeholder="Mật khẩu"
-        iconClassName="bi bi-unlock"
-      />
-      <MyInput
-        className="mb-3"
-        type="password"
-        placeholder="Xác nhận mật khẩu"
-        iconClassName="bi bi-unlock"
-      />
-
-      <MyButton className="w-100 fw-medium text-white">Đăng ký</MyButton>
+            <MyButton
+              className="w-100 fw-medium text-white"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Đăng ký
+            </MyButton>
+          </Form>
+        )}
+      </Formik>
 
       <div className="text-secondary pt-4">
         <div onClick={() => router.push('/sign-in')}>
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const API_URL = process.env.API_URL
+console.log('API_URL', API_URL)
