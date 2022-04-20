22bb689aba9ab70a0a70b58610ad1491efabe20b
Dang Khoa <nguyendangkhoa200400@gmail.com>
2022-04-20 11:34:23 +0700

    Lobby when host a quiz and join a quiz

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -22,3 +22,41 @@ export type TApiResponse<T> = {
   message: string
   response: T
 }
+
+export type TPlayer = {
+  id: number
+
+  // gameLobby:
+
+  // gameLobby: object
+
+  gameLobbyId: number
+
+  nickname: string
+
+  user?: TUser
+
+  userId?: number
+
+  score: number
+}
+
+export type TGameLobby = {
+  id?: number
+
+  players: TPlayer[]
+
+  // quiz: Quiz
+
+  quizId: number
+
+  // host: User
+
+  hostId: number
+
+  mode: string
+
+  time: number
+
+  invitationCode: string
+}
