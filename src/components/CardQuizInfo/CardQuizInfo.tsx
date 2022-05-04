beb41815f3fc5f5d423a4fff4f62945858173517
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2022-05-04 23:33:54 +0700

    add update quiz

--- src/components/CardQuizInfo/CardQuizInfo.tsx
+++ src/components/CardQuizInfo/CardQuizInfo.tsx
@@ -1,21 +1,35 @@
+import { Formik, Field, Form } from 'formik'
+import { useRouter } from 'next/router'
 import React, { FC, useState } from 'react'
-import { Col, Image, Placeholder, Row } from 'react-bootstrap'
-import { TQuiz } from '../../types/types'
+import { Col, FormCheck, Image, Placeholder, Row } from 'react-bootstrap'
+import { post } from '../../libs/api'
+import { TApiResponse, TQuiz } from '../../types/types'
 import {
   getUrl,
   storage,
   storageRef,
   uploadFile,
 } from '../../utils/firebaseConfig'
+import MyButton from '../MyButton/MyButton'
+import MyInput from '../MyInput/MyInput'
+import MyModal from '../MyModal/MyModal'
 import QuestionActionButton from '../QuestionActionButton/QuestionActionButton'
 
 type CardQuizInfoProps = {
   quiz: TQuiz | undefined
   isValidating: boolean
+  setQuiz: React.Dispatch<React.SetStateAction<TQuiz | undefined>>
 }
 
-const CardQuizInfo: FC<CardQuizInfoProps> = ({ quiz, isValidating }) => {
+const CardQuizInfo: FC<CardQuizInfoProps> = ({
+  quiz,
+  isValidating,
+  setQuiz,
+}) => {
   const [bannerUrl, setBannerUrl] = useState<string>('')
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const router = useRouter()
+  const quizId = Number(router.query.id)
 
   const handleUploadImage = async (evt: any) => {
     try {
@@ -92,9 +106,135 @@ const CardQuizInfo: FC<CardQuizInfoProps> = ({ quiz, isValidating }) => {
           </div>
         </Col>
         <Col xs="auto">
-          <QuestionActionButton iconClassName="bi bi-pencil" />
+          <QuestionActionButton
+            iconClassName="bi bi-pencil"
+            onClick={() => setShowModal(true)}
+          />
         </Col>
       </Row>
+
+      <MyModal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        header={<div className="fs-24px fw-medium">Chỉnh sửa Quiz</div>}
+      >
+        <Formik
+          initialValues={{
+            title: quiz?.title ?? '',
+            description: quiz?.description ?? '',
+            isPublic: quiz?.isPublic ?? false,
+          }}
+          onSubmit={async (value, actions) => {
+            try {
+              if (!quiz) return
+
+              const body = { ...quiz, ...value, banner: bannerUrl }
+              const res = await post<TApiResponse<TQuiz>>(
+                `/api/quizzes/${quizId}`,
+                {},
+                body,
+                true
+              )
+
+              setQuiz(res.response)
+            } catch (error) {
+              console.log('onSaveQuestion - error', error)
+            } finally {
+              actions.setSubmitting(false)
+            }
+          }}
+          // validationSchema={ProfileSchema}
+        >
+          {({ handleSubmit, isSubmitting }) => (
+            <Form
+              method="POST"
+              onSubmit={(e) => {
+                e.preventDefault()
+                handleSubmit()
+              }}
+            >
+              <div
+                className="border rounded-10px position-relative overflow-hidden"
+                style={{ height: 120 }}
+              >
+                {bannerUrl.length ? (
+                  <Image
+                    src={bannerUrl}
+                    alt=""
+                    width="100%"
+                    height="100%"
+                    className="object-fit-cover"
+                  />
+                ) : (
+                  <div className="py-4 text-center fs-14px text-secondary">
+                    <div className="bi bi-image text-primary fs-32px"></div>
+                    Bấm hoặc kéo thả tại đây để thêm ảnh bìa
+                  </div>
+                )}
+                <input
+                  type="file"
+                  onChange={handleUploadImage}
+                  onDropCapture={handleUploadImage}
+                  className="position-absolute top-0 w-100 h-100 opacity-0 cursor-pointer"
+                  accept="image/png, image/jpeg, image/jpg"
+                  style={{ left: 0 }}
+                />
+              </div>
+              <Row className="justify-content-center align-items-center py-2">
+                <Col xs={12} lg={4} className="text-lg-end fw-medium">
+                  Tên
+                </Col>
+                <Col>
+                  <Field
+                    type="text"
+                    name="title"
+                    placeholder="Tên"
+                    as={MyInput}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="justify-content-center align-items-center py-2">
+                <Col xs={12} lg={4} className="text-lg-end fw-medium">
+                  Mô tả
+                </Col>
+                <Col>
+                  <Field
+                    type="text"
+                    name="description"
+                    placeholder="Mô tả"
+                    as={MyInput}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="justify-content-center align-items-center py-2">
+                <Col xs={12} lg={4} className="text-lg-end fw-medium">
+                  Công khai
+                </Col>
+                <Col>
+                  <Field
+                    type="switch"
+                    name="isPublic"
+                    placeholder="Công khai"
+                    as={FormCheck}
+                  />
+                </Col>
+              </Row>
+
+              <div className="text-center pt-3">
+                <MyButton
+                  className="text-white w-100"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Lưu thông tin
+                </MyButton>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </MyModal>
     </div>
   )
 }
