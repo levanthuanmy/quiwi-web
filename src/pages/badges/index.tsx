7cf177caa587aec32d8cf1a57f19545b83655570
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2022-05-14 18:46:48 +0700

    update ui

--- src/pages/badges/index.tsx
+++ src/pages/badges/index.tsx
@@ -1,130 +1,162 @@
-import { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
-import { Col, Collapse, Container, Row, Modal } from 'react-bootstrap'
-import { useAuth } from '../../hooks/useAuth/useAuth'
-import MenuBar from '../../components/MenuBar/MenuBar'
-import NavBar from '../../components/NavBar/NavBar'
-import { HOME_MENU_OPTIONS } from '../../utils/constants'
 import classNames from 'classnames'
+import { NextPage } from 'next'
+import React, { useState } from 'react'
+import { Col, Modal, Row } from 'react-bootstrap'
 import CardBadge from '../../components/CardBadge/CardBadge'
-import QuestItem from '../../components/QuestItem/QuestItem'
+import DashboardLayout from '../../components/DashboardLayout/DashboardLayout'
 import ModalBadge from '../../components/ModalBadge/ModalBadge'
+import QuestItem from '../../components/QuestItem/QuestItem'
 
 const BadgesPage: NextPage = () => {
-  const [isExpand, setIsExpand] = useState<boolean>(false)
-  const authNavigate = useAuth()
   const [toggleState, setToggleState] = useState<number>(1)
-
-  const toggleTab = (index: number) => {
-    setToggleState(index)
-  }
-
-  const [show, setShow] = useState(false);
-
-  const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false)
+  const handleShow = () => setShow(true)
 
   return (
-    <>
-      <NavBar />
-      <div className="d-flex pt-64px min-vh-100">
-        <MenuBar
-          isExpand={isExpand}
-          setIsExpand={setIsExpand}
-          menuOptions={HOME_MENU_OPTIONS}
-          isFullHeight={true}
-        />
-        <div className="ps-5 w-100 transition-all-150ms">
+    <DashboardLayout>
+      <div className="w-100">
+        <Row
+          className="justify-content-md-center"
+          style={{ paddingBottom: '30px' }}
+        >
+          <Col sm={8}>
+            <nav className="nav flex-column flex-sm-row ">
+              <a
+                className={classNames(
+                  'flex-sm-fill text-sm-center nav-link navbar-quest',
+                  {
+                    'active-tab-with-green': toggleState === 1,
+                    'tab-with-green': toggleState !== 1,
+                  }
+                )}
+                onClick={() => setToggleState(1)}
+              >
+                Đã sở hữu
+              </a>
+              <a
+                className={classNames(
+                  'flex-sm-fill text-sm-center nav-link navbar-quest',
+                  {
+                    'active-tab-with-green': toggleState === 2,
+                    'tab-with-green': toggleState !== 2,
+                  }
+                )}
+                onClick={() => setToggleState(2)}
+              >
+                Chưa sở hữu
+              </a>
+            </nav>
+          </Col>
+        </Row>
+
+        {toggleState === 2 ? (
+          <div>
+            <Row
+              className="justify-content-md-center"
+              style={{ paddingBottom: '17px' }}
+            >
+              <Col sm={9}>
+                <QuestItem
+                  currentValue={3}
+                  targetValue={5}
+                  name={'Top 1 thần thánh'}
+                  des={'Có 5 lần đạt top 1'}
+                  isDone={false}
+                ></QuestItem>
+              </Col>
+            </Row>
+            <Row
+              className="justify-content-md-center"
+              style={{ paddingBottom: '17px' }}
+            >
+              <Col sm={9}>
+                <QuestItem
+                  currentValue={1}
+                  targetValue={1}
+                  name={'Top 3 thần thánh'}
+                  des={'Có 1 lần đạt top 3'}
+                  isDone={true}
+                ></QuestItem>
+              </Col>
+            </Row>
+            <Row
+              className="justify-content-md-center"
+              style={{ paddingBottom: '17px' }}
+            >
+              <Col sm={9}>
+                <QuestItem
+                  currentValue={1}
+                  targetValue={5}
+                  name={'Top 2 thần thánh'}
+                  des={'Có 5 lần đạt top 2'}
+                  isDone={false}
+                ></QuestItem>
+              </Col>
+            </Row>
+            <Row
+              className="justify-content-md-center"
+              style={{ paddingBottom: '17px' }}
+            >
+              <Col sm={9}>
+                <QuestItem
+                  currentValue={10}
+                  targetValue={10}
+                  name={'Hoàn thành 10 bài quiz'}
+                  des={'Hoàn thành đầy đủ 10 bài quiz'}
+                  isDone={false}
+                ></QuestItem>
+              </Col>
+            </Row>
+          </div>
+        ) : toggleState === 1 ? (
           <Row
             className="justify-content-md-center"
-            style={{ paddingBottom: '30px' }}
+            style={{ paddingBottom: '17px' }}
           >
-            <Col sm={8}>
-              <nav className="nav flex-column flex-sm-row ">
-                <a
-                  className={classNames(
-                    'flex-sm-fill text-sm-center nav-link navbar-quest',
-                    {
-                      'active-tab-with-green': toggleState === 1,
-                      'tab-with-green': toggleState !== 1,
-                    }
-                  )}
-                  onClick={() => setToggleState(1)}
-                >
-                  Đã sở hữu
-                </a>
-                <a
-                  className={classNames(
-                    'flex-sm-fill text-sm-center nav-link navbar-quest',
-                    {
-                      'active-tab-with-green': toggleState === 2,
-                      'tab-with-green': toggleState !== 2,
-                    }
-                  )}
-                  onClick={() => setToggleState(2)}
-                >
-                  Chưa sở hữu
-                </a>
-              </nav>
+            <Col sm={9}>
+              <div className={classNames('badges-list')}>
+                <CardBadge
+                  onClick={handleShow}
+                  image={
+                    'https://img.freepik.com/free-vector/knight-shield-cartoon-sticker_1308-66058.jpg'
+                  }
+                  title={'Khiêng chúa'}
+                  description={'Dùng khiêng bảo hộ 100 lần'}
+                  propgress={'100'}
+                ></CardBadge>
+                <CardBadge
+                  onClick={handleShow}
+                  image={
+                    'https://cdn.imgbin.com/22/1/23/imgbin-police-officer-cartoon-police-badge-material-yellow-police-badge-art-z25Etc1QUL7BnaGV70qy0HWBv.jpg'
+                  }
+                  title={'Huân Chương Top 1'}
+                  description={'Đạt top 1 500 lần'}
+                  propgress={'60'}
+                ></CardBadge>
+                <CardBadge
+                  onClick={handleShow}
+                  image={
+                    'https://freepikpsd.com/file/2019/10/cartoon-diamond-png-3-Transparent-Images.png'
+                  }
+                  title={'Thánh kim cương'}
+                  description={'Đạt được 1000 viên kim cương'}
+                  propgress={'30'}
+                ></CardBadge>
+                <CardBadge
+                  onClick={handleShow}
+                  image={
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkSN0audfR-FYWM-pnfcysnC4QBjbXucVTrA&usqp=CAU'
+                  }
+                  title={'Chuyên gia cày cuốc'}
+                  description={
+                    'Đạt top 3 trong 10 trận gần nhất không dùng vật phẩm'
+                  }
+                  propgress={'10'}
+                ></CardBadge>
+              </div>
             </Col>
           </Row>
-
-          {toggleState === 2 ? (
-            <div>
-              <Row className="justify-content-md-center" style={{ paddingBottom: "17px" }}>
-                <Col sm={9} >
-                  <QuestItem currentValue={3} targetValue={5} name={'Top 1 thần thánh'} des={'Có 5 lần đạt top 1'} isDone={false}></QuestItem></Col>
-              </Row>
-              <Row className="justify-content-md-center" style={{ paddingBottom: "17px" }}>
-                <Col sm={9} >
-                  <QuestItem currentValue={1} targetValue={1} name={'Top 3 thần thánh'} des={'Có 1 lần đạt top 3'} isDone={true}></QuestItem></Col>
-              </Row>
-              <Row className="justify-content-md-center" style={{ paddingBottom: "17px" }}>
-                <Col sm={9} >
-                  <QuestItem currentValue={1} targetValue={5} name={'Top 2 thần thánh'} des={'Có 5 lần đạt top 2'} isDone={false}></QuestItem></Col>
-              </Row>
-              <Row className="justify-content-md-center" style={{ paddingBottom: "17px" }}>
-                <Col sm={9} >
-                  <QuestItem currentValue={10} targetValue={10} name={'Hoàn thành 10 bài quiz'} des={'Hoàn thành đầy đủ 10 bài quiz'} isDone={false}></QuestItem></Col>
-              </Row>
-            </div>
-          ) : toggleState === 1 ?
-            (
-              (
-                <Row className="justify-content-md-center" style={{ paddingBottom: "17px" }}>
-                  <Col sm={9} >
-                    <div className={classNames('badges-list')}>
-                      <CardBadge
-                        onClick={handleShow}
-                        image={'https://img.freepik.com/free-vector/knight-shield-cartoon-sticker_1308-66058.jpg'}
-                        title={'Khiêng chúa'}
-                        description={'Dùng khiêng bảo hộ 100 lần'}
-                        propgress={'100'}></CardBadge>
-                      <CardBadge
-                        onClick={handleShow}
-                        image={'https://cdn.imgbin.com/22/1/23/imgbin-police-officer-cartoon-police-badge-material-yellow-police-badge-art-z25Etc1QUL7BnaGV70qy0HWBv.jpg'}
-                        title={'Huân Chương Top 1'}
-                        description={'Đạt top 1 500 lần'}
-                        propgress={'60'}></CardBadge>
-                      <CardBadge
-                        onClick={handleShow}
-                        image={'https://freepikpsd.com/file/2019/10/cartoon-diamond-png-3-Transparent-Images.png'}
-                        title={'Thánh kim cương'}
-                        description={'Đạt được 1000 viên kim cương'}
-                        propgress={'30'}></CardBadge>
-                      <CardBadge
-                        onClick={handleShow}
-                        image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkSN0audfR-FYWM-pnfcysnC4QBjbXucVTrA&usqp=CAU'}
-                        title={'Chuyên gia cày cuốc'}
-                        description={'Đạt top 3 trong 10 trận gần nhất không dùng vật phẩm'}
-                        propgress={'10'}></CardBadge>
-                    </div>
-                  </Col>
-                </Row>
-              )
-            )
-            : null}
-        </div>
+        ) : null}
       </div>
 
       <Modal
@@ -134,9 +166,9 @@ const BadgesPage: NextPage = () => {
         onShow={() => setShow(true)}
         onHide={() => setShow(false)}
       >
-        <ModalBadge onClose={() => setShow(false)} ></ModalBadge>
+        <ModalBadge onClose={() => setShow(false)}></ModalBadge>
       </Modal>
-    </>
+    </DashboardLayout>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,13 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import MenuBar from '../components/MenuBar/MenuBar'
+import DashboardLayout from '../components/DashboardLayout/DashboardLayout'
 import MyButton from '../components/MyButton/MyButton'
 import MyInput from '../components/MyInput/MyInput'
-import NavBar from '../components/NavBar/NavBar'
-import { useAuth } from '../hooks/useAuth/useAuth'
 import { post } from '../libs/api'
 import { TApiResponse, TQuiz, TQuizBodyRequest } from '../types/types'
-import { HOME_MENU_OPTIONS } from '../utils/constants'
 
 const Home: NextPage = () => {
-  const [isExpand, setIsExpand] = useState<boolean>(false)
-  const authNavigate = useAuth()
   const [invitationCode, setInvitationCode] = useState<string>('')
   const router = useRouter()
 
@@ -51,157 +46,148 @@ const Home: NextPage = () => {
   }
 
   return (
-    <>
-      <NavBar />
-      <div className="d-flex pt-64px min-vh-100">
-        <MenuBar
-          isExpand={isExpand}
-          setIsExpand={setIsExpand}
-          menuOptions={HOME_MENU_OPTIONS}
-          isFullHeight={true}
-        />
-        <div className="ps-5 w-100 transition-all-150ms bg-secondary bg-opacity-10">
-          <div className="bg-white">
-            <Container fluid="lg" className="p-3">
-              <Row>
-                <Col xs="12" lg="8" className="pe-lg-2 pb-3 pb-lg-0">
-                  <div className="border rounded-10px p-3">
-                    <div className="fs-22px fw-medium pb-4">
-                      Tham gia một quiz
-                    </div>
-
-                    <Row>
-                      <Col xs="12" sm="6" className="pe-sm-2 pb-3 pb-sm-0">
-                        <MyInput
-                          placeholder="Nhập mã tham gia"
-                          onChange={(e) => {
-                            setInvitationCode(e.target.value)
-                          }}
-                        />
-                      </Col>
-                      <Col xs="12" sm="4" xl="3" className="ps-sm-2">
-                        <MyButton
-                          className="fw-medium text-white w-100"
-                          onClick={onJoinRoom}
-                        >
-                          Tham gia ngay
-                        </MyButton>
-                      </Col>
-                    </Row>
-                  </div>
-                </Col>
-
-                <Col xs="12" lg="4" className="ps-lg-2">
-                  <div className="border rounded-10px p-3">
-                    <div className="fs-22px fw-medium pb-4 w-100">
-                      Tạo một quiz
-                    </div>
-                    <MyButton
-                      className="fw-medium text-white"
-                      onClick={handleToQuizCreator}
-                    >
-                      Tạo mới ngay
-                    </MyButton>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </div>
-
+    <DashboardLayout>
+      <div className="w-100 bg-secondary bg-opacity-10">
+        <div className="bg-white">
           <Container fluid="lg" className="p-3">
-            <div className="pt-4">
-              <div className="fs-22px fw-medium pb-3">Đã tham gia gần đây</div>
-              <Row className="overflow-auto flex-nowrap">
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-              </Row>
-            </div>
+            <Row>
+              <Col xs="12" lg="8" className="pe-lg-2 pb-3 pb-lg-0">
+                <div className="border rounded-10px p-3">
+                  <div className="fs-22px fw-medium pb-4">
+                    Tham gia một quiz
+                  </div>
 
-            <div className="pt-4">
-              <div className="fs-22px fw-medium pb-3">Đã tạo gần đây</div>
-              <Row className="overflow-auto flex-nowrap">
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-              </Row>
-            </div>
+                  <Row>
+                    <Col xs="12" sm="6" className="pe-sm-2 pb-3 pb-sm-0">
+                      <MyInput
+                        placeholder="Nhập mã tham gia"
+                        onChange={(e) => {
+                          setInvitationCode(e.target.value)
+                        }}
+                      />
+                    </Col>
+                    <Col xs="12" sm="4" xl="3" className="ps-sm-2">
+                      <MyButton
+                        className="fw-medium text-white w-100"
+                        onClick={onJoinRoom}
+                      >
+                        Tham gia ngay
+                      </MyButton>
+                    </Col>
+                  </Row>
+                </div>
+              </Col>
 
-            <div className="pt-4">
-              <div className="fs-22px fw-medium pb-3">Phổ biến</div>
-              <Row className="overflow-auto flex-nowrap">
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-                <Col xs="auto">
-                  <div
-                    style={{ width: 278, height: 240 }}
-                    className="border rounded-10px bg-white"
-                  ></div>
-                </Col>
-              </Row>
-            </div>
+              <Col xs="12" lg="4" className="ps-lg-2">
+                <div className="border rounded-10px p-3">
+                  <div className="fs-22px fw-medium pb-4 w-100">
+                    Tạo một quiz
+                  </div>
+                  <MyButton
+                    className="fw-medium text-white"
+                    onClick={handleToQuizCreator}
+                  >
+                    Tạo mới ngay
+                  </MyButton>
+                </div>
+              </Col>
+            </Row>
           </Container>
         </div>
+
+        <Container fluid="lg" className="p-3">
+          <div className="pt-4">
+            <div className="fs-22px fw-medium pb-3">Đã tham gia gần đây</div>
+            <Row className="overflow-auto flex-nowrap">
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+            </Row>
+          </div>
+
+          <div className="pt-4">
+            <div className="fs-22px fw-medium pb-3">Đã tạo gần đây</div>
+            <Row className="overflow-auto flex-nowrap">
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+            </Row>
+          </div>
+
+          <div className="pt-4">
+            <div className="fs-22px fw-medium pb-3">Phổ biến</div>
+            <Row className="overflow-auto flex-nowrap">
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+              <Col xs="auto">
+                <div
+                  style={{ width: 278, height: 240 }}
+                  className="border rounded-10px bg-white"
+                ></div>
+              </Col>
+            </Row>
+          </div>
+        </Container>
       </div>
-    </>
+    </DashboardLayout>
   )
 }
 
