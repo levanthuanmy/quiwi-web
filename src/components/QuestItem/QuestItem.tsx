adb3a5fdd1bc78326e8a8fe2438d3c99bbbfc20d
huylydang <huyly@kobiton.com>
2022-06-05 16:49:09 +0700

    add api for quest

--- src/components/QuestItem/QuestItem.tsx
+++ src/components/QuestItem/QuestItem.tsx
@@ -4,6 +4,8 @@ import { Col, Container, Row, Image } from "react-bootstrap"
 import styles from './QuestItem.module.css'
 import classNames from 'classnames'
 import MyButton from "../MyButton/MyButton"
+import { TQuest } from '../../types/types'
+import Item from '../Item/Item'
 
 type IQuestItem = {
   currentValue: number,
@@ -14,16 +16,14 @@ type IQuestItem = {
 }
 
 
-const QuestItem: FC<IQuestItem> = (props) => {
-  const [doneState, setDoneState] = useState<boolean>(props.isDone);
+const QuestItem: FC<{ props: TQuest }> = (props) => {
+  // const [doneState, setDoneState] = useState<boolean>(props.isDone);
 
-  const toggleTab = (value: boolean) => {
-    if (props.currentValue === props.targetValue)
-      setDoneState(value);
-  }
+  // const toggleTab = (value: boolean) => {
+  //   if (props.currentValue === props.targetValue)
+  //     setDoneState(value);
+  // }
 
-  const progress = props.currentValue / props.targetValue * 100
-  console.log(progress)
 
   return (
     <div>
@@ -35,56 +35,62 @@ const QuestItem: FC<IQuestItem> = (props) => {
             'card',
             styles.questItem
           )}>
-            <div className="card-body quest-item">
-              <Row>
-                <Col sm={7}><h5 className={classNames("card-title", styles.fontTitle)}>{props.name}</h5>
+            <div className={classNames('card-body quest-item ')}>
+              <Row className={classNames('', styles.price)}>
+                <Col sm={7}><h5 className={classNames("card-title", styles.fontTitle)}>{props.props.questTitle}</h5>
                 </Col>
-                <Col sm={2}>
+                <Col sm={2} className={classNames("", styles.price)}>
                   <Row>
-                    <Col sm={3}>
-                      <Image
-                        src=
-                        "https://img.freepik.com/free-vector/knight-shield-cartoon-sticker_1308-66058.jpg"
-                        roundedCircle
-                        width={25}
-                        height={25}
-                        alt="coin"
-                      ></Image>
-                    </Col>
-                    <Col sm={7}>
-                      <div>
-                        x100</div></Col>
+
+                    {props.props.questGoal
+                      .filter(item => item.type === "00COIN")
+                      .map((item, idx) => (
+                        <Row >
+                          <Col sm={1}>
+                            <img alt="avatar" src="/assets/quiwi-coin.png" width="20" height="20"></img>
+
+                          </Col>
+                          <Col >
+                            <div>
+                              {item.reward.coin}
+                            </div></Col>
+                        </Row>
+                      ))}
                   </Row>
                 </Col>
-                <Col sm={2}>
-                  <Row>
-                    <Col sm={3}>
-                      <Image
-                        src=
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQsYgFkhCp0lUJBQPbI7SGr_PNkkWb-wH6ag&usqp=CAU"
-                        roundedCircle
-                        width={25}
-                        height={25}
-                        alt="coin"
-                      ></Image>
-                    </Col>
-                    <Col sm={7}>
-                      <div>
-                        x100</div></Col>
-                  </Row>
-                </Col>
+              </Row>
+              {/* <p className={classNames("card-text", styles.fontDes)}>{props.props.detail}</p> */}
+
+
+
+              <Row className={classNames('justify-content-md-center"', styles.listItem)}>
+                {props.props.questGoal
+                  .filter(item => item.type !== "00COIN")
+                  .map((item, idx) => (
+                    <Col sm={1}><div className={classNames('', styles.layoutImage)}>
+                      <Item
+                        key={idx}
+                        name={item.reward.name}
+                        des={item.reward.description}
+                        avatar={item.reward.avatar}
+                        type={item.reward.type}
+                        price={item.reward.price}
+                      ></Item>
+                    </div> </Col>
+                  ))}
 
               </Row>
-              <p className={classNames("card-text", styles.fontDes)}>{props.des}</p>
+
               <div className="progress">
-                <div className={classNames("progress-bar progress-bar-striped bg-success")} role="progressbar" style={{ width: progress.toString() + "%" }}></div>
+                <div className={classNames("progress-bar progress-bar-striped bg-success")} role="progressbar" style={{ width: props.props.userQuest[0].process.toString() + "%" }}></div>
               </div>
-              <p className="card-text text-end fs-20px">{props.currentValue.toString() + "/" + props.targetValue.toString()}</p>
+              <p className="card-text text-end fs-20px">{props.props.userQuest[0].process.toString()}%</p>
             </div>
           </div>
         </Col>
         <Col sm={2}>
-          <MyButton disabled={!(progress === 100 && !doneState)} className={classNames("w-100 text-white")} onClick={() => toggleTab(true)}>{doneState ? "Đã nhận" : progress === 100 ? "Nhận" : "Chưa xong" }</MyButton>
+          {/* <MyButton disabled={!(progress === 100 && !doneState)} className={classNames("w-100 text-white")} onClick={() => toggleTab(true)}>{doneState ? "Đã nhận" : progress === 100 ? "Nhận" : "Chưa xong" }</MyButton> */}
+          <MyButton className={classNames("w-100 text-white")} >{props.props.userQuest[0].status}</MyButton>
         </Col>
       </Row>
     </div>
