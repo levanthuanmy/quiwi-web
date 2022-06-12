babf4866695230e668dc38ef89a4a6cc12949aca
my.le <my.le>
2022-06-12 19:03:59 +0700

    prefetching some resources

--- src/components/MyHead/MyHead.tsx
+++ src/components/MyHead/MyHead.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import React, { FC } from 'react'
+import { API_URL } from '../../utils/constants'
 
 const MyHead: FC = () => {
   return (
@@ -7,6 +8,18 @@ const MyHead: FC = () => {
       <link rel="icon" href="/assets/logo.png" />
       <link type="image/x-icon" rel="shortcut icon" href="/assets/logo.png" />
       <link rel="apple-touch-icon" href="/assets/logo.png"></link>
+
+      <link
+        rel="preconnect"
+        href="https://firebasestorage.googleapis.com/v0/b/quiwi-511fc.appspot.com/o/"
+      />
+      <link
+        rel="dns-prefetch"
+        href="https://firebasestorage.googleapis.com/v0/b/quiwi-511fc.appspot.com/o/"
+      />
+
+      <link rel="preconnect" href={API_URL} />
+      <link rel="dns-prefetch" href={API_URL} />
       <title>Quiwi Game</title>
     </Head>
   )
