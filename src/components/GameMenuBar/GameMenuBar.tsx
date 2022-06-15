d5a08e63dd2973335babe08d5317aa5aad7b917e
Thiện <thiennc5@vng.com.vn>
2022-06-16 00:38:51 +0700

    add toast chat

--- src/components/GameMenuBar/GameMenuBar.tsx
+++ src/components/GameMenuBar/GameMenuBar.tsx
@@ -6,6 +6,7 @@ import ChatWindow from '../GameComponents/ChatWindow/ChatWindow'
 import { MessageProps } from '../GameComponents/ChatWindow/Message'
 import PlayerList from '../GameComponents/PlayerList/PlayerList'
 import styles from './GameMenuBar.module.css'
+import {useToasts} from "react-toast-notifications";
 
 type GameMenuBarProps = {
   gameSession: TStartQuizResponse
@@ -15,15 +16,19 @@ type GameMenuBarProps = {
 const GameMenuBar: FC<GameMenuBarProps> = ({ gameSession, isShow }) => {
   const [chatContent, setChatContent] = useState<MessageProps[]>([])
   const socket = SocketManager().socketOf('GAMES')
-
+  const { addToast } = useToasts()
   const receivedMessage = (message: MessageProps) => {
     if (message) {
       setChatContent([...chatContent, message])
     }
   }
-
+  socket?.off('chat')
   socket?.on('chat', (data: MessageProps) => {
     receivedMessage(data)
+    addToast(`${data.name}: ${data.message}`, {
+      appearance: 'info',
+      autoDismiss: true,
+    })
   })
 
   const renderItems = (
