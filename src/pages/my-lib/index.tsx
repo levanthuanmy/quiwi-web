b5b684c38f8940fb0eac399780e0530d7e1614b3
Dang Khoa <nguyendangkhoa200400@gmail.com>
2022-06-19 18:00:36 +0700

    [WIP] Fix pagination for get all quizzes

--- src/pages/my-lib/index.tsx
+++ src/pages/my-lib/index.tsx
@@ -1,27 +1,32 @@
 import { NextPage } from 'next'
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import useSWR from 'swr'
 import DashboardLayout from '../../components/DashboardLayout/DashboardLayout'
 import ItemQuiz from '../../components/ItemQuiz/ItemQuiz'
+import { MyPagination } from '../../components/MyPagination/MyPagination'
 import { get } from '../../libs/api'
 import { TApiResponse, TPaginationResponse, TQuiz } from '../../types/types'
 
 const MyLibPage: NextPage = () => {
+  const pageSize = 2
+  const [pageIndex, setPageIndex] = useState(1)
   const params = {
     filter: {
       relations: ['questions', 'questions.questionAnswers'],
-      order: {createdAt: 'DESC'}
+      order: { createdAt: 'DESC' },
     },
-    pageIndex: 1,
-    pageSize: 100,
+    pageIndex: pageIndex,
+    pageSize: pageSize,
   }
   const { data, isValidating } = useSWR<
     TApiResponse<TPaginationResponse<TQuiz>>
   >(['/api/quizzes/my-quizzes', true, params], get, {
     revalidateOnFocus: false,
   })
-
+  const handlePageClick = (selected: { selected: number }) => {
+    setPageIndex(Number(selected.selected) + 1)
+  }
   return (
     <DashboardLayout>
       <div className="w-100 bg-secondary bg-opacity-10 min-vh-100">
@@ -40,6 +45,14 @@ const MyLibPage: NextPage = () => {
               ))}
             </Row>
           )}
+          <Row className="mt-3">
+            <Col style={{ display: 'flex', justifyContent: 'center' }}>
+              <MyPagination
+                handlePageClick={handlePageClick}
+                totalPages={data?.response.totalPages ?? 0}
+              />
+            </Col>
+          </Row>
         </Container>
       </div>
     </DashboardLayout>
