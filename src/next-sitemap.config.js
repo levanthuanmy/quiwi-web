7086d567af0221c1873f9f67e4a97c0c74cb5e5c
levanthuanmy <levanthuanmy2000@gmail.com>
2022-06-20 17:20:44 +0000

    update next sitemap

--- src/next-sitemap.config.js
+++ src/next-sitemap.config.js
@@ -18,15 +18,15 @@ const config = {
   //   },
   // ],
   // Default transformation function
-  transform: async (config, path) => {
-    return {
-      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    }
-  },
+  // transform: async (config, path) => {
+  //   return {
+  //     loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+  //     changefreq: config.changefreq,
+  //     priority: config.priority,
+  //     lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+  //     alternateRefs: config.alternateRefs ?? [],
+  //   }
+  // },
   // additionalPaths: async (config) => [
   //   await config.transform(config, '/additional-page'),
   // ],
@@ -53,4 +53,4 @@ const config = {
   },
 }
 
-export default config
+module.exports = config
