d3c270613262f101cd43fe2a16d475cd2c25a6c9
Dang Khoa <nguyendangkhoa200400@gmail.com>
2022-06-22 00:06:45 +0700

    Fix get user in quest, fix layout quest list

--- src/components/ListQuest/ListQuest.tsx
+++ src/components/ListQuest/ListQuest.tsx
@@ -1,150 +1,167 @@
 import classNames from 'classnames'
 import { NextPage } from 'next'
-import React, { useRef, useState, useEffect } from 'react'
-import { Col, Container, Row, Modal } from 'react-bootstrap'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Col, Container, Modal, Row } from 'react-bootstrap'
 import DashboardLayout from '../../components/DashboardLayout/DashboardLayout'
 import MyTabBar from '../../components/MyTabBar/MyTabBar'
 import QuestItem from '../../components/QuestItem/QuestItem'
 import { useAuth } from '../../hooks/useAuth/useAuth'
-import { get, post } from '../../libs/api'
-import { TApiResponse, TQuest, TPaginationResponse } from '../../types/types'
-import styles from './ListQuest.module.css'
-import SearchBar from '../SearchBar/SearchBar'
-import { forEach } from 'lodash'
+import { get } from '../../libs/api'
+import { TApiResponse, TPaginationResponse, TQuest } from '../../types/types'
 import MyModal from '../MyModal/MyModal'
+import SearchBar from '../SearchBar/SearchBar'
+import styles from './ListQuest.module.css'
 
 const ListQuest: NextPage = () => {
-    const [toggleState, setToggleState] = useState<number>(0)
-    const [itemsResponse, setItemsResponse] =
-        useState<TPaginationResponse<TQuest>>()
-    const [doneQuests, setDoneQuests] = useState<TQuest[]>()
-    const [inProgressQuests, setInProgressQuests] = useState<TQuest[]>()
-    const authContext = useAuth()
-    const [showDialog, setShowDialog] = useState<boolean>(false)
-    const onShowDialog = () => {
-        setShowDialog(true)
-        getItemCategories()
-    }
+  const [toggleState, setToggleState] = useState<number>(0)
+  const router = useRouter()
+  const { q } = router.query
 
-    const getItemCategories = async () => {
-        if (authContext !== undefined) {
-            let currentUserId = authContext.getUser()?.id || null
-            try {
-                const popularParams = {
-                    filter: {
-                        relations: ['questRequirement', 'questGoal', 'userQuest'],
+  const [itemsResponse, setItemsResponse] =
+    useState<TPaginationResponse<TQuest>>()
+  const [doneQuests, setDoneQuests] = useState<TQuest[]>()
+  const [inProgressQuests, setInProgressQuests] = useState<TQuest[]>()
+  const authContext = useAuth()
+  const user = authContext.getUser()
+  const [showDialog, setShowDialog] = useState<boolean>(false)
 
-                        where: {
-                            userQuest: {
-                                userId: currentUserId,
-                            },
-                        },
-                    },
-                }
+  const onShowDialog = () => {
+    setShowDialog(true)
+    getItemCategories()
+  }
 
-                const res: TApiResponse<TPaginationResponse<TQuest>> = await get(
-                    `/api/quest/all`,
-                    true,
-                    popularParams
-                )
-                if (res.response) {
-                    let iDoneQuests: TQuest[] = []
-                    let iInProgressQuests: TQuest[] = []
-                    setItemsResponse(res.response)
-                    res.response.items.forEach((element) => {
-                        if (element.userQuest[0].isReceivedReward)
-                            iDoneQuests.push(element)
-                        else
-                            iInProgressQuests.push(element)
-                    })
-                    setDoneQuests(iDoneQuests)
-                    setInProgressQuests(iInProgressQuests)
+  const getItemCategories = async () => {
+    if (user) {
+      let currentUserId = user.id
+      try {
+        const popularParams = {
+          filter: {
+            relations: ['questRequirement', 'questGoal', 'userQuest'],
 
-                }
-            } catch (error) {
-                alert('Có lỗi nè')
-                console.log(error)
-            }
+            where: {
+              userQuest: {
+                userId: currentUserId,
+              },
+            },
+          },
+          q,
         }
+
+        const res: TApiResponse<TPaginationResponse<TQuest>> = await get(
+          `/api/quest/all`,
+          true,
+          popularParams
+        )
+        if (res.response) {
+          let iDoneQuests: TQuest[] = []
+          let iInProgressQuests: TQuest[] = []
+          setItemsResponse(res.response)
+          res.response.items.forEach((element) => {
+            if (element.userQuest[0].isReceivedReward) iDoneQuests.push(element)
+            else iInProgressQuests.push(element)
+          })
+          setDoneQuests(iDoneQuests)
+          setInProgressQuests(iInProgressQuests)
+        }
+      } catch (error) {
+        alert('Có lỗi nè')
+        console.log(error)
+      }
     }
-    useEffect(() => {
-        
+  }
 
-        getItemCategories()
-        // if (!itemsResponse){
-        //   getItems()
-        // }
-    }, [])
+  useEffect(() => {
+    getItemCategories()
 
-    const tabOptions = [
-        'Nhiệm vụ đang làm',
-        'Nhiệm vụ đã hoàn thành',
-        'Nhiệm vụ tháng',
-        'Nhiệm vụ đặc biệt',
-    ]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, q])
 
-    return (
-        <DashboardLayout>
-            <Container fluid="lg" className={classNames("p-3", styles.disableScroll)}>
-                <div className="" >
-                    <MyTabBar
-                        currentTab={toggleState}
-                        setCurrentTab={setToggleState}
-                        tabs={tabOptions}
-                    />
-                    <Row className={classNames("justify-content-center", styles.searchBar)}>
-                        <Col xs={12} sm={12} md={8}>
-                            <SearchBar
-                                pageUrl="quests"
-                                inputClassName="border border-primary"
-                            />
-                        </Col>
+  const tabOptions = [
+    'Nhiệm vụ đang làm',
+    'Nhiệm vụ đã hoàn thành',
+    'Nhiệm vụ tháng',
+    'Nhiệm vụ đặc biệt',
+  ]
 
-                    </Row>
-                    <Row
-                        className={classNames("justify-content-center m-0 my-4", styles.board)}
-                    >
-                        <Col xs={12} sm={12} md={8} className={classNames("", styles.ListQuest)} >
-                            {toggleState === 0 ? (
-                                <>
-                                    {inProgressQuests?.map((item, idx) => (
-                                        <Col key={idx} className="p-0 mb-3">
-                                            <QuestItem quest={item} onClick={() => onShowDialog()}></QuestItem>
-                                        </Col>
-                                    ))}
-                                </>
-                            ) : toggleState === 1 ? (
-                                <>
-                                    {doneQuests?.map((item, idx) => (
-                                        <Col key={idx} className="p-0 mb-3">
-                                            <QuestItem quest={item} onClick={() => onShowDialog()}></QuestItem>
-                                        </Col>
-                                    ))}
-                                </>
-                            ) : null}
-                        </Col>
-                    </Row>
-                </div>
-                <MyModal
-                    show={showDialog}
-                    onHide={() => {
-                        setShowDialog(false)
-                    }}
-                    activeButtonTitle="Xác nhận"
-                    activeButtonCallback={() => {
-                        setShowDialog(false)
-                    }}
-                    size="sm"
-                    header={<Modal.Title className={'text-primary'}>Xác nhận</Modal.Title>}
-                >
-                    <div className="text-center fw-medium">
-                        Chúc mừng bạn đã hoàn thành nhiệm vụ{' '}
-                    </div>
-                </MyModal>
-            </Container>
-        </DashboardLayout>
-
-    )
+  return (
+    <DashboardLayout>
+      <Container fluid="lg" className={classNames('p-3', styles.disableScroll)}>
+        <div className="">
+          <MyTabBar
+            currentTab={toggleState}
+            setCurrentTab={setToggleState}
+            tabs={tabOptions}
+          />
+          <Row
+            className={classNames('justify-content-center', styles.searchBar)}
+          >
+            <Col xs={12} sm={12} md={8}>
+              <SearchBar
+                pageUrl="quests"
+                inputClassName="border border-primary"
+              />
+            </Col>
+          </Row>
+          <Row
+            className={classNames(
+              'justify-content-center m-0 my-4',
+              styles.board
+            )}
+          >
+            <Col
+              xs={12}
+              sm={12}
+              md={10}
+              className={classNames('p-2', styles.ListQuest)}
+            >
+              {toggleState === 0 ? (
+                <>
+                  {inProgressQuests?.map((item, idx) => (
+                    <Col key={idx} className="p-0 mb-3">
+                      <QuestItem
+                        quest={item}
+                        onClick={() => onShowDialog()}
+                      ></QuestItem>
+                    </Col>
+                  ))}
+                </>
+              ) : toggleState === 1 ? (
+                <>
+                  {doneQuests?.map((item, idx) => (
+                    <Col key={idx} className="p-0 mb-3">
+                      <QuestItem
+                        quest={item}
+                        onClick={() => onShowDialog()}
+                      ></QuestItem>
+                    </Col>
+                  ))}
+                </>
+              ) : null}
+            </Col>
+          </Row>
+        </div>
+        <MyModal
+          show={showDialog}
+          onHide={() => {
+            setShowDialog(false)
+          }}
+          activeButtonTitle="Xác nhận"
+          activeButtonCallback={() => {
+            setShowDialog(false)
+          }}
+          size="sm"
+          header={
+            <Modal.Title className={'text-primary'}>Xác nhận</Modal.Title>
+          }
+        >
+          <div className="text-center fw-medium">
+            Chúc mừng bạn đã hoàn thành nhiệm vụ
+          </div>
+        </MyModal>
+      </Container>
+    </DashboardLayout>
+  )
 }
 
 export default ListQuest
