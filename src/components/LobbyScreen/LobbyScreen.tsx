76f8701db78fe7ba28e338795f0d724fb8a7c9f7
my.le <my.le>
2022-06-30 18:24:54 +0700

    update layout lobby, show user, show quiz in lobby

--- src/components/LobbyScreen/LobbyScreen.tsx
+++ src/components/LobbyScreen/LobbyScreen.tsx
@@ -1,28 +1,28 @@
 /* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable react-hooks/exhaustive-deps */
+import classNames from 'classnames'
+import { Howl } from 'howler'
 import _ from 'lodash'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { FC, useEffect, useState } from 'react'
-import { Modal } from 'react-bootstrap'
+import { Container, Image, Modal } from 'react-bootstrap'
 import QRCode from 'react-qr-code'
 import { useToasts } from 'react-toast-notifications'
 import Cookies from 'universal-cookie'
+import { useAuth } from '../../hooks/useAuth/useAuth'
 import { useGameSession } from '../../hooks/useGameSession/useGameSession'
 import { useLocalStorage } from '../../hooks/useLocalStorage/useLocalStorage'
 import useScreenSize from '../../hooks/useScreenSize/useScreenSize'
 import * as gtag from '../../libs/gtag'
-import { TPlayer, TStartGameRequest, TUser } from '../../types/types'
-import { JsonParse } from '../../utils/helper'
+import { TPlayer, TStartGameRequest } from '../../types/types'
+import { GAME_MODE_MAPPING, SOUND_EFFECT } from '../../utils/constants'
 import MyButton from '../MyButton/MyButton'
 import MyModal from '../MyModal/MyModal'
 import PlayerLobbyItem from '../PlayerLobbyItem/PlayerLobbyItem'
 import PlayerLobbyList from '../PlayerLobbyList/PlayerLobbyList'
 import BackgroundPicker from './BackgroundPicker/BackgroundPicker'
 import styles from './LobbyScreen.module.css'
-import { playSound, playSoundWithLoop } from '../../utils/helper';
-import { SOUND_EFFECT } from '../../utils/constants';
-import { Howl } from 'howler'
 
 type LobbyScreenProps = {
   invitationCode: string
@@ -30,9 +30,7 @@ type LobbyScreenProps = {
 }
 const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
   const [playerList, setPlayerList] = useState<TPlayer[]>([])
-  const [lsUser] = useLocalStorage('user', '')
   const [lsBg, saveLsBg] = useLocalStorage('bg', '')
-  const [user, setUser] = useState<TUser>()
   const router = useRouter()
   const [showQR, setShowQR] = useState<boolean>(false)
   const { isMobile } = useScreenSize()
@@ -52,9 +50,11 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
     new Howl({
       src: SOUND_EFFECT['GAME_WATING'],
       html5: true,
-      loop: true
+      loop: true,
     })
-  );
+  )
+  const authContext = useAuth()
+  const user = authContext.getUser()
 
   useEffect(() => {
     if (!gameSession) return
@@ -88,29 +88,26 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
     })
 
     gameSkOnce('host-out', () => {
-      sound.stop();
+      sound.stop()
       router.push('/')
     })
 
     gameSkOnce('loading', (data) => {
       // console.log('game started', data)
-      sound.stop();
+      sound.stop()
       router.push(`/game/play`)
     })
 
     gameSkOn('error', (data) => {
-      sound.stop();
-      console.log('LobbyScreen.tsx - error', data)
+      sound.stop()
     })
   }, [gameSession])
 
   useEffect(() => {
-    sound.play();
-    if (lsUser) setUser(JsonParse(lsUser) as TUser)
-    if(lsBg && lsBg.length) {
+    sound.play()
+    if (lsBg && lsBg.length) {
       setCurrentBackground(lsBg)
-    }
-    else {
+    } else {
       setCurrentBackground('/assets/default-game-bg.svg')
       saveLsBg('/assets/default-game-bg.svg')
     }
@@ -118,18 +115,18 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
 
   useEffect(() => {
     router.beforePopState(({ as }) => {
-        if (as !== router.asPath) {
-            // Will run when leaving the current page; on back/forward actions
-            // Add your logic here, like toggling the modal state
-            sound.stop();
-        }
-        return true;
-    });
+      if (as !== router.asPath) {
+        // Will run when leaving the current page; on back/forward actions
+        // Add your logic here, like toggling the modal state
+        sound.stop()
+      }
+      return true
+    })
 
     return () => {
-        router.beforePopState(() => true);
-    };
-}, [router]);
+      router.beforePopState(() => true)
+    }
+  }, [router])
 
   const handleLeaveRoom = () => {
     clearGameSession()
@@ -198,6 +195,7 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
       <>
         <div className={styles.gameOfHost}>
           <PlayerLobbyItem
+            avatar={gameSession?.host?.avatar}
             isHost={true}
             displayName={'Chủ phòng: ' + hostName}
             bgColor={'#009883'}
@@ -211,16 +209,13 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
     <div
       style={{
         backgroundImage: `url(${currentBackground}`,
-        // backgroundSize: 'auto 100vh',
+        backgroundSize: 'auto auto',
         backgroundRepeat: 'repeat',
         backgroundPosition: 'center',
       }}
       className="bg-dark"
     >
-      <div
-        className="bg-dark bg-opacity-50 fw-medium bg-opacity-10 min-vh-100 d-flex flex-column justify-content-center align-items-center"
-        // style={{ backdropFilter: 'blur(3px)' }}
-      >
+      <div className="bg-dark bg-opacity-50 fw-medium bg-opacity-10 min-vh-100 d-flex flex-column justify-content-center align-items-center">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -234,13 +229,77 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
           />
         </Head>
 
-        {isMobile ? renderInvitationCode() : renderPcInvitationCode()}
+        <Container fluid="md" className="p-3 px-md-5">
+          <div className="bg-dark d-flex flex-column justify-content-center align-items-center rounded-10px">
+            <div className="d-flex w-100 align-items-center gap-3 p-3">
+              <Image
+                src={
+                  authContext.isAuth ? user?.avatar : '/assets/default-user.png'
+                }
+                width="60"
+                height="60"
+                roundedCircle
+                alt="avatar"
+              />
+              <div className="text-white h3 text-truncate">
+                <div>
+                  {authContext.isAuth ? user?.name : gameSession?.nickName}
+                </div>
+                <div className="fs-14px text-primary text-wrap">
+                  {!authContext.isAuth && (
+                    <>
+                      Đăng nhập ngay để nhận phần thưởng và nhiều tính năng khác
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
 
-        {renderHostName()}
+            <div className="text-white d-flex w-100 align-items-center gap-3 bg-black bg-opacity-50 p-3">
+              <div className="w-100">
+                <div className="text-truncate">
+                  Tên quiz: {gameSession?.quiz?.title}
+                </div>
+                <div className="">
+                  Số câu: {gameSession?.quiz?.questions?.length}
+                </div>
+                <div>
+                  Chế độ chơi:{' '}
+                  {GAME_MODE_MAPPING[gameSession?.mode || '10CLASSIC']}
+                </div>
+              </div>
+              {isHost && (
+                <MyButton size="sm" className="text-white text-nowrap">
+                  Chi tiết quiz
+                </MyButton>
+              )}
+            </div>
 
-        {renderPlayerList()}
-        {/*Thoát + Bắt đầu*/}
-        {functionalButtons()}
+            {isMobile ? renderInvitationCode() : renderInvitationCodeDesktop()}
+
+            {renderHostName()}
+
+            {/* player list */}
+            <div className="p-3 w-100 text-center">
+              <div className={styles.playerCount}>
+                {playerList?.length || 0} người tham gia!
+              </div>
+              <div
+                className="d-flex flex-wrap align-items-center justify-content-center bg-white rounded-10px"
+                style={{
+                  minHeight: 100,
+                  overflowX: 'hidden',
+                  overflowY: 'auto',
+                }}
+              >
+                <PlayerLobbyList players={playerList} />
+              </div>
+            </div>
+
+            {/*Thoát + Bắt đầu*/}
+            {functionalButtons()}
+          </div>
+        </Container>
 
         {/*Các view ẩn*/}
         {renderQrModal()}
@@ -254,76 +313,64 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
     </div>
   )
 
-  /*QR code nếu trên màn bự*/
-  function renderPcInvitationCode() {
-    return (
-      <div className={styles.invitationCodeContainer}>
-        <>
-          {/*QR code*/}
-          <div className={'d-flex flex-column align-items-center gap-3'}>
-            <div className={styles.qrContainer}>
-              <QRCode
-                size={200}
-                fgColor={'#009883'}
-                bgColor={'#F0F1F2'}
-                value={`http://${window.location.host}/lobby/join?invitationCode=${invitationCode}`}
-                className={styles.qrModal}
-              />
-            </div>
-            <div>Quét mã để tham gia</div>
-          </div>
-
-          {/*Khoảng cách ở giữa*/}
-          <span
-            className={
-              'position-relative text-secondary text-opacity-75 flex-grow-1 align-self-end'
-            }
-          >
-            {' hoặc '}
-          </span>
-
-          {/*Mã phòng và copy link*/}
-          <div className={'d-flex flex-column align-items-center gap-3'}>
-            {/*Mã mời*/}
-            <div
-              className={
-                'd-flex flex-column justify-content-center flex-grow-1'
-              }
-            >
-              <span className={styles.code}>{invitationCode}</span>
-            </div>
-            {/*Copy*/}
-            <div>
-              Tham gia bằng
-              <span className={styles.joinLink} onClick={copyInvitationCode}>
-                {' link '}
-                <i className={`bi bi-clipboard-plus-fill`} />
-              </span>
-            </div>
-          </div>
-        </>
-      </div>
-    )
-  }
-
   function renderInvitationCode() {
     return (
       <>
-        <div className={'d-flex gap-3'}>
-          <span className={styles.code}>{invitationCode}</span>
-          <i
-            className={`bi bi-qr-code-scan ${styles.copyIcon}`}
+        <div className="text-white p-3 w-100 d-flex justify-content-center align-items-center flex-column">
+          <div>
+            <div className=" h3">Mã mời:</div>
+          </div>
+          <div className={styles.code}>{invitationCode}</div>
+          <div>hoặc Bấm </div>
+          <MyButton
+            className="bi bi-qr-code-scan fs-24px my-2 text-white"
             onClick={() => {
               setShowQR(true)
             }}
           />
+          <div className="text-center text-wrap">
+            để nhận mã QR và đường dẫn vào phòng
+          </div>
         </div>
-        <div className={'pt-12px'}>
-          Copy
-          <span className={styles.joinLink} onClick={copyInvitationCode}>
-            {' link vào phòng '}
-            <i className={`bi bi-clipboard-plus-fill`} />
-          </span>
+      </>
+    )
+  }
+
+  function renderInvitationCodeDesktop() {
+    return (
+      <>
+        <div className="text-white p-3 d-flex justify-content-center gap-4 w-100 flex-wrap">
+          <div className="d-flex flex-column h-100">
+            <div className="h5">Quét mã QR để vào phòng</div>
+            <div className="bg-white p-2 text-center rounded-6px">
+              <QRCode
+                size={200}
+                fgColor={'#009883'}
+                value={`http://${window.location.host}/lobby/join?invitationCode=${invitationCode}`}
+                className={styles.qrModal}
+              />
+            </div>
+          </div>
+
+          <div className="d-flex flex-column gap-3 text-center">
+            <div>
+              <div className="h5">Mã mời</div>
+              <div className={styles.code}>{invitationCode}</div>
+            </div>
+
+            <div>
+              <div className="h5">Bấm để tạo đường dẫn vào phòng</div>
+              <div
+                className="d-flex w-100 bg-primary bg-opacity-10 rounded-8px p-3 align-items-center gap-3 cursor-pointer"
+                onClick={copyInvitationCode}
+              >
+                <div className="text-truncate w-100">
+                  {`http://${window.location.host}/lobby/join?invitationCode=${invitationCode}`}{' '}
+                </div>
+                <div className={`bi bi-clipboard-plus-fill fs-24px`} />
+              </div>
+            </div>
+          </div>
         </div>
       </>
     )
@@ -331,73 +378,67 @@ const LobbyScreen: FC<LobbyScreenProps> = ({ invitationCode, isHost }) => {
 
   function renderQrModal() {
     return (
-      <Modal
+      <MyModal
         show={showQR}
         onHide={() => {
           setShowQR(false)
         }}
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
+        header={<Modal.Title>Tham gia phòng</Modal.Title>}
       >
-        <Modal.Header closeButton className={styles.modalHeader}>
-          <Modal.Title
-            id="contained-modal-title-vcenter"
-            className={styles.modalTitle}
+        <div className="d-flex justify-content-center flex-column align-items-center gap-3">
+          <div className="h5">Quét mã QR để vào phòng</div>
+          <QRCode
+            size={200}
+            fgColor={'#009883'}
+            value={`http://${window.location.host}/lobby/join?invitationCode=${invitationCode}`}
+            className={styles.qrModal}
+          />
+          <div className="h5 text-secondary pt-3">hoặc</div>
+          <div className="h5">Bấm để tạo đường dẫn vào phòng</div>
+          <div
+            className="d-flex w-100 bg-primary bg-opacity-10 rounded-8px p-3 align-items-center gap-3 cursor-pointer"
+            onClick={copyInvitationCode}
           >
-            Quét mã QR để vào phòng
-          </Modal.Title>
-        </Modal.Header>
-        <QRCode
-          size={200}
-          fgColor={'#009883'}
-          value={`http://${window.location.host}/lobby/join?invitationCode=${invitationCode}`}
-          className={styles.qrModal}
-        />
-      </Modal>
+            <div className="text-truncate w-100">
+              {`http://${window.location.host}/lobby/join?invitationCode=${invitationCode}`}{' '}
+            </div>
+            <div className={`bi bi-clipboard-plus-fill fs-24px`} />
+          </div>
+        </div>
+      </MyModal>
     )
   }
 
   function functionalButtons() {
+    const cn = classNames(
+      'text-white fw-medium d-flex align-items-center gap-2 w-100',
+      { 'justify-content-center': isMobile }
+    )
     return (
-      <div className="d-flex gap-3 p-12px flex-wrap">
-        <MyButton
-          variant="danger"
-          className="fw-medium d-flex align-items-center gap-2"
-          onClick={handleLeaveRoom}
-        >
-          <i className="bi bi-door-open fs-24px" />
-          RỜI PHÒNG
-        </MyButton>
-        <MyButton
-          className="text-white fw-medium d-flex align-items-center gap-2"
-          onClick={() => setShowBackgroundModal(true)}
-        >
-          <i className="bi bi-image fs-24px" />
-          CHỌN ẢNH NỀN
-        </MyButton>
-        {isHost && (
-          <MyButton
-            className="text-white fw-medium d-flex align-items-center gap-2"
-            onClick={handleStartGame}
-          >
-            <i className="bi bi-play-fill fs-24px" />
-            BẮT ĐẦU
+      <div className="d-flex gap-3 px-3 pb-3 flex-wrap w-100">
+        <div className="flex-fill">
+          <MyButton variant="danger" className={cn} onClick={handleLeaveRoom}>
+            <i className="bi bi-box-arrow-left fs-24px" />
+            {!isMobile && 'RỜI PHÒNG'}
           </MyButton>
+        </div>
+
+        <div className="flex-fill">
+          <MyButton className={cn} onClick={() => setShowBackgroundModal(true)}>
+            <i className="bi bi-image fs-24px" />
+            {!isMobile && 'CHỌN HÌNH NỀN'}
+          </MyButton>
+        </div>
+
+        {isHost && (
+          <div className="flex-fill">
+            <MyButton className={cn} onClick={handleStartGame}>
+              <i className="bi bi-play-fill fs-24px" />
+              BẮT ĐẦU
+            </MyButton>
+          </div>
         )}
       </div>
-    )
-  }
-
-  function renderPlayerList() {
-    return (
-      <>
-        <div className={styles.playerCount}>
-          {playerList.length} người tham gia!
-        </div>
-        <div className={`d-flex flex-wrap ${styles.playerList}`}>
-          <PlayerLobbyList players={playerList} />
-        </div>
-      </>
     )
   }
 }
