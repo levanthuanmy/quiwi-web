218c9d136ee34f128f5b1871b2e0e6880dedaf67
huylydang <huyly@kobiton.com>
2022-07-21 00:35:15 +0700

    Update ProfileInformation.tsx

--- src/components/ProfileInformation/ProfileInformation.tsx
+++ src/components/ProfileInformation/ProfileInformation.tsx
@@ -6,7 +6,6 @@ const ProfileInformation: FC<{
   return (
     <div className="h-100">
       <div className="fs-32px fw-medium">{user.name}</div>
-      <div className="text-secondary fs-16px">@{user.username}</div>
       {user.isVerified ? (
         <div className="mt-1 text-success fw-bold">
           Tài khoản đã được xác thực <i className="bi bi-check-lg"></i>
